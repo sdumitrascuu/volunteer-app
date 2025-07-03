8feef6bddfa17f6a0895b6c99329597ddea2529a
sdumitrascuu <samardumitrascu@gmail.com>
2025-07-03 00:41:45 -0500

    Update App.jsx

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import AdminDashboard from './pages/AdminDash';
 import VolunteerDashboard from './pages/VolunteerDash';
+import VolunteerMatch from './pages/VolunteerMatch';
+import AdminNotifications from './pages/AdminNotifications';
+import EventHistory from './pages/EventHistory';
 
 function App() {
   return (
@@ -10,9 +13,13 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/volunteer" element={<VolunteerDashboard />} />
+        <Route path="/match" element={<VolunteerMatch />} />
+        <Route path="/notifications" element={<AdminNotifications />} />
+        <Route path="/history" element={<EventHistory />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
