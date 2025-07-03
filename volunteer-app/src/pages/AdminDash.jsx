e5359178bd05605af80ae313a2b3a4cc90ab49b8
sdumitrascuu <samardumitrascu@gmail.com>
2025-07-03 00:21:12 -0500

    Update AdminDash.jsx

--- volunteer-app/src/pages/AdminDash.jsx
+++ volunteer-app/src/pages/AdminDash.jsx
@@ -1,44 +1,71 @@
-import { Container, Typography, Button, Table, TableHead, TableRow, TableCell, TableBody,Fab} from '@mui/material';
+import React from 'react';
+import {
+  Container,
+  Typography,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Fab,
+} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import EventForm from '../components/EventForm';
 
 export default function AdminDash() {
-    const events = [
-        {id:1, title:'Food Drive', date: '2024-07-01', location:'Midtown', volunteers:5},
-        {id: 2, title: 'Clothing Distribution', date:'2024-07-05', location:'East Side', volunteers:10},
-    ];
+  const [events, setEvents] = React.useState([
+    { id: 1, title: 'Food Drive', date: '2024-07-01', location: 'Midtown', volunteers: 5 },
+    { id: 2, title: 'Clothing Distribution', date: '2024-07-05', location: 'East Side', volunteers: 10 },
+  ]);
 
-    return (
-        <Container sx = {{ mt:4}}>
-            <Typography variant = "h4" gutterBottom>
-                Admin Dashboard
-            </Typography>
+  const [showForm, setShowForm] = React.useState(false);
 
-            {/* Events Table*/}
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Event Title</TableCell>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Location</TableCell>
-                        <TableCell>Volunteers Needed</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {events.map(event => (
-                        <TableRow Key = {event.id}>
-                            <TableCell>{event.title}</TableCell>
-                            <TableCell>{event.date}</TableCell>
-                            <TableCell>{event.location}</TableCell>
-                            <TableCell>{event.volunteers}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            {/* Add Event Button*/}
-            <Fab color = "primary" aria-label = "add" sx = {{ position: 'fixed', bottom:24, right: 24}}>
-                <AddIcon />
-            </Fab>
-        </Container>
-    );
+  const handleAddEvent = (newEvent) => {
+    setEvents((prevEvents) => [...prevEvents, { ...newEvent, id: prevEvents.length + 1 }]);
+    setShowForm(false);
+  };
+
+  return (
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Admin Dashboard
+      </Typography>
+
+      {/* Events Table */}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Event Title</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Location</TableCell>
+            <TableCell>Volunteers Needed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {events.map((event) => (
+            <TableRow key={event.id}>
+              <TableCell>{event.title}</TableCell>
+              <TableCell>{event.date}</TableCell>
+              <TableCell>{event.location}</TableCell>
+              <TableCell>{event.volunteers}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      {/* Show Form if Toggled */}
+      {showForm && <EventForm onClose={() => setShowForm(false)} onSubmit={handleAddEvent} />}
+
+      {/* Add Event Button */}
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{ position: 'fixed', bottom: 24, right: 24 }}
+        onClick={() => setShowForm(true)}
+      >
+        <AddIcon />
+      </Fab>
+    </Container>
+  );
 }
-  
